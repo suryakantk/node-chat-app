c2591d6c89a5b5ddc54be9789b32ed9ff4950e01
suryakantk <suryakant.k@gmail.com>
2018-01-20 16:03:57 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+const express = require('express');
+
+var app =express();
+
+const publicPath = path.join(__dirname,'../public');
+const port=process.env.PORT || 3000;
+
+
+app.use(express.static(publicPath));
+
+app.listen(3000,()=>{
+ console.log('wellcome');
+
+});
+
+//
+// console.log(publicPath);
+// console.log(__dirname + '../public');
+
+//
+// const path=require('path');
+// const express=require('express');
+//
+// const publicPath=path.join(__dirname,'../public');
+// const port=process.env.PORT || 3000;
+//
+// var app=express();
+//
+// app.use(express.static(publicPath));
+//
+// app.listen(3000,()=>{
+//  console.log('wellcome');
+//
+// });
+//
+// console.log(__dirname + '../public');
+// console.log();
